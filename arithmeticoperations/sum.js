132beb010415776ee678d3f4911a25eca3d27cef
muhammedshebinva <muhammedshebinva@gmail.com>
2021-04-17 13:43:23 +0530

    code for Sum of two numbers

---
+++ arithmeticoperations/sum.js
@@ -0,0 +1,9 @@
+var read=require('readline-sync')
+//console.log('enter 2 numbers')
+
+var num1=read.question('enetr the numbers')
+var num2=read.question('')
+
+var sum=Number(num1)+Number(num2)
+
+console.log('sum='+sum)
